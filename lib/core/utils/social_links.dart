2332d27436657ee042a307f01a3681b1e9568d1e
sackoba <badrasacko11@gmail.com>
2025-02-17 16:09:07 +0000

    config and add assets to the project

---
+++ lib/core/utils/social_links.dart
@@ -0,0 +1,6 @@
+abstract class SocialLinks {
+  static const String facebook = "https://www.facebook.com/";
+  static const String twitter = "https://twitter.com/";
+  static const String instagram = "https://www.instagram.com/";
+  static const String linkedin = "https://www.linkedin.com/in/";
+}
---
+++ lib/root.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/view/home_view.dart';
+
+class Root extends StatelessWidget {
+  const Root({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
+  }
+}
